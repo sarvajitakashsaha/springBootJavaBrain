76942f8d97c8f55620851aebdf8badecee75f13d
sarvajit saha <saha.sarvajit@gmail.com>
2020-05-27 18:18:21 +0530

    adding interceptor

---
+++ springbootjavabrain/src/main/java/com/example/springboot/topic/TopicConfig.java
@@ -0,0 +1,22 @@
+package com.example.springboot.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class TopicConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private RequestHeaderIntercepter requestHeaderIntercepter;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addInterceptor(requestHeaderIntercepter);
+		
+	}
+	
+	
+} 
